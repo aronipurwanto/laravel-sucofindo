ee50d64e5f24c7933520736b9772d33b87e6c0e6
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-20 16:19:03 +0700

    feat: singleton and bind

---
+++ laravel-dasar/app/Data/Person.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Data;
+
+class Person
+{
+    private string $firstName;
+    private string $lastName;
+
+    /**
+     * @param string $firstName
+     * @param string $lastName
+     */
+    public function __construct(string $firstName, string $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+}
--- laravel-dasar/tests/Feature/DependencyInjectionTest.php
+++ laravel-dasar/tests/Feature/DependencyInjectionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Data\Bar;
 use App\Data\Foo;
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -38,6 +39,43 @@ class DependencyInjectionTest extends TestCase
         self::assertNotSame($foo, $bar->getFoo());
         self::assertNotSame($foo, $foo2);
         self::assertNotSame($bar, $bar2);
+    }
+
+    public function testBind()
+    {
+        $this->app->bind(Person::class, function ($app){
+            return new Person("Ahmad","Roni");
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        assertEquals("Ahmad", $person1->getFirstName());
+        assertEquals("Ahmad", $person2->getFirstName());
+        assertEquals($person1->getFirstName(), $person2->getFirstName());
+
+        self::assertNotSame($person1, $person2);
+    }
+
+    public function testSingleton()
+    {
+        $this->app->singleton(Person::class, function ($app){
+            return new Person("Ahmad","Roni");
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+        $person3 = $this->app->make(Person::class);
+
+        assertEquals("Ahmad", $person1->getFirstName());
+        assertEquals("Ahmad", $person2->getFirstName());
+        assertEquals("Ahmad", $person3->getFirstName());
+
+        assertEquals($person1->getFirstName(), $person2->getFirstName());
+
+        self::assertSame($person1, $person2);
+        self::assertSame($person2, $person3);
+        self::assertSame($person1, $person3);
     }
 
 
