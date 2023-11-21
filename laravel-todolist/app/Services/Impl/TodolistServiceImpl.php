caffd9aa6a928fec55926be302fadd928d67078c
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-21 14:20:04 +0700

    feat: add todolist

---
+++ laravel-todolist/app/Services/Impl/TodolistServiceImpl.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\Impl;
+
+use App\Services\TodolistService;
+
+class TodolistServiceImpl implements TodolistService
+{
+
+    public function saveTodo(string $id, string $todo): void
+    {
+        // TODO: Implement saveTodo() method.
+    }
+
+    public function getTodolist(): array
+    {
+        // TODO: Implement getTodolist() method.
+    }
+
+    public function removeTodo($todoId): void
+    {
+        // TODO: Implement removeTodo() method.
+    }
+}
