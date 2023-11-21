b02717bc7ff3a38466fa078df0ff814b7a214163
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-21 09:54:11 +0700

    feat: logic login

---
+++ laravel-todolist/app/Services/Impl/UserServiceImpl.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Impl;
+
+use App\Services\UserServices;
+
+class UserServiceImpl implements UserServices
+{
+    private $users = [
+        "nana" => "anonimous"
+    ];
+
+    public function login(string $username, string $password): bool
+    {
+        if(!isset($username) || !isset($password)){
+            return false;
+        }
+
+        if(!$this->users[$username]){
+            return false;
+        }
+
+        $correctPassword = $this->users[$username];
+        if($correctPassword == $password){
+            return true;
+        }
+    }
+}
