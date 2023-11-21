b92b58c5ec4a8404a4a45d3d1b4177b426e81524
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-21 14:35:32 +0700

    feat: todolist service

--- laravel-todolist/app/Services/Impl/TodolistServiceImpl.php
+++ laravel-todolist/app/Services/Impl/TodolistServiceImpl.php
@@ -3,22 +3,38 @@
 namespace App\Services\Impl;
 
 use App\Services\TodolistService;
+use Illuminate\Support\Facades\Session;
 
 class TodolistServiceImpl implements TodolistService
 {
 
     public function saveTodo(string $id, string $todo): void
     {
-        // TODO: Implement saveTodo() method.
+        if(!Session::exists("todolist")){
+            Session::put("todolist",[]);
+        }
+
+        Session::push("todolist",[
+            "id" => $id,
+            "todo"=>$todo
+        ]);
     }
 
     public function getTodolist(): array
     {
-        // TODO: Implement getTodolist() method.
+        return Session::get("todolist",[]);
     }
 
     public function removeTodo($todoId): void
     {
-        // TODO: Implement removeTodo() method.
+        $todolist = Session::get("todolist");
+        foreach ($todolist as $index => $value){
+            if($todoId == $value['id']){
+                unset($todolist[$index]);
+                break;
+            }
+        }
+
+        Session::put('todolist', $todolist);
     }
 }
