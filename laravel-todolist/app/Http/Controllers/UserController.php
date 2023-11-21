590fde479079cafce854cee9cfc001db5a366942
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-21 11:22:20 +0700

    feat: MF-6881 validate input when client_phone is null

--- laravel-todolist/app/Http/Controllers/UserController.php
+++ laravel-todolist/app/Http/Controllers/UserController.php
@@ -2,21 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserServices;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
+    private UserServices $userServices;
+
+    /**
+     * @param UserServices $userServices
+     */
+    public function __construct(UserServices $userServices)
+    {
+        $this->userServices = $userServices;
+    }
+
+
     public function login() : Response
     {
         return response()
-            ->view('user.login');
+            ->view('user.login',['title' => 'Login Page']);
     }
 
-    public function doLogin():Response | RedirectResponse
+    public function doLogin(Request $request):Response | RedirectResponse
     {
+        $username = $request->input('user');
+        $password = $request->input('password');
 
+        if(!isset($username) || !isset($password)){
+            return response()
+                ->view('user.login',[
+                    'title'=>'Login Page',
+                    'error' => 'Username or password is empty'
+                ]);
+        }
+
+        if($this->userServices->login($username, $password)) {
+            return redirect('/');
+        }
+
+        return response()
+            ->view('user.login',[
+                'title'=>'Login Page',
+                'error' => 'Username or password not valid'
+            ]);
     }
 
     public function logout(): RedirectResponse
---
+++ laravel-todolist/tests/Feature/UserControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    public function testLoginPage()
+    {
+        $this->get('/login')
+            ->assertSeeText("Login")
+            ->assertSeeText("Password")
+            ->assertSeeText("User");
+    }
+
+}
