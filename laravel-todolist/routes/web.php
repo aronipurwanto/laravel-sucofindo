971879ffd3d1e6b53970a3e14670b56e9bf9c2ad
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-21 10:46:46 +0700

    feat: route login

--- laravel-todolist/routes/web.php
+++ laravel-todolist/routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/template',function (){
+    return view('template');
+});
+
+/**
+Route::get('/login',[\App\Http\Controllers\UserController::class,'login']);
+Route::post('/login',[\App\Http\Controllers\UserController::class,'doLogin']);
+Route::post('/login',[\App\Http\Controllers\UserController::class,'logout']);
+ */
+
+Route::controller(\App\Http\Controllers\UserController::class)
+    ->group(function (){
+        Route::get('/login','login');
+        Route::post('/login','doLogin');
+        Route::post('/logout','logout');
+    });
