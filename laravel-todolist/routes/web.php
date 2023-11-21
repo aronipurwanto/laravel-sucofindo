6498c0f13751fd710b69aa34da80c633c17343f4
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-21 15:08:09 +0700

    Route Home Controller

--- laravel-todolist/routes/web.php
+++ laravel-todolist/routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
 Route::get('/', function () {
     return view('welcome');
 });
+ */
+Route::get('/', [\App\Http\Controllers\HomeController::class,'home']);
 
 Route::get('/template',function (){
     return view('template');
