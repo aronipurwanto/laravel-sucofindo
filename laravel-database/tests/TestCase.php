a546d35add67d37110bd5d57fff806b6f86236cb
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-22 15:56:13 +0700

    update: category

--- laravel-database/tests/TestCase.php
+++ laravel-database/tests/TestCase.php
@@ -12,6 +12,19 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
+        DB::delete("delete from taggables");
+        DB::delete("delete from tags");
+        DB::delete("delete from employees");
+        DB::delete("delete from persons");
+        DB::delete("delete from reviews");
+        DB::delete("delete from images");
+        DB::delete("delete from customers_likes_products");
+        DB::delete("delete from products");
         DB::delete("delete from categories");
+        DB::delete("delete from vouchers");
+        DB::delete("delete from comments");
+        DB::delete("delete from virtual_accounts");
+        DB::delete("delete from wallets");
+        DB::delete("delete from customers");
     }
 }
